0d92c3e888320b5f484290874b89337d2a86fce4
ほと <mail@hoto.me>
2015-11-18 00:16:55 +0900

    use request module

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var lambda = require('gulp-awslambda');
 var zip    = require('gulp-zip');
 
 gulp.task('default', function() {
-  return gulp.src('index.js')
+  return gulp.src(['./**/*', '!archive.zip'])
     .pipe(zip('archive.zip'))
     .pipe(lambda('gotandajs-slack', { region: 'ap-northeast-1' }))
     .pipe(gulp.dest('.'));
--- index.js
+++ index.js
@@ -1,23 +1,37 @@
-var https = require('https');
+var request = require('request');
+
+var api_base;
 
 exports.handler = function(event, context) {
-
   if ( ! event.slack_team || ! event.slack_token ) {
     context.fail(new Error('undefined slack env'));
     return;
   }
-  var url = 'https://' + event.slack_team + '.slack.com/api/users.list?presence=1&token=' + event.slack_token;
+  api_base = 'https://' + event.slack_team + '.slack.com/api';
 
-  https.get(url, function(res) {
-    var body = '';
-    res.on('data', function(chunk){ body += chunk; });
-    res.on('end', function(res){
-      var info = {};
-      var json = JSON.parse(body);
-      var users = json.members.filter(function(user) { return ! user.is_bot && ! user.deleted; });
-      info.total = users.length;
-      info.online = users.filter(function(user) { return user.presence === 'active'; }).length;
-      context.succeed(info);
-    });
-  });
+  switch ( event.method ) {
+    case 'GET':  get(event, context);  break;
+    case 'POST': post(event, context); break;
+  }
 };
+
+function get(event, context) {
+  var params = {
+    url: api_base + '/users.list',
+    qs: {
+      token: event.slack_token,
+      presence: 1,
+    },
+  };
+  request.get(params, function(err, response, body) {
+    var info = {};
+    var json = JSON.parse(body);
+    var users = json.members.filter(function(user) { return ! user.is_bot && ! user.deleted; });
+    info.total = users.length;
+    info.online = users.filter(function(user) { return user.presence === 'active'; }).length;
+    context.succeed(info);
+  });
+}
+
+function post(event, context) {
+}
