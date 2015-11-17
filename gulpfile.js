0589e30c4d3c597581ca07436cc0f80d9a3430ab
ほと <mail@hoto.me>
2015-11-17 22:52:22 +0900

    deploy to lambda

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,10 @@
-var gulp = require('gulp');
+var gulp   = require('gulp');
+var lambda = require('gulp-awslambda');
+var zip    = require('gulp-zip');
+
+gulp.task('default', function() {
+  return gulp.src('index.js')
+    .pipe(zip('archive.zip'))
+    .pipe(lambda('gotandajs-slack', { region: 'ap-northeast-1' }))
+    .pipe(gulp.dest('.'));
+});
