04425c8c0bd90b389d715562c781594a163263b3
ほと <mail@hoto.me>
2015-11-21 15:33:35 +0900

    招待メール送信

--- index.js
+++ index.js
@@ -9,23 +9,23 @@ exports.handler = function(event, context) {
   if ( ! event.action || typeof( actions[ event.action ] ) !== 'function' ) {
     return context.fail(new Error('undefined action'));
   }
-  api_base = 'https://' + event.slack_team + '.slack.com/api';
+  api_base = 'https://' + event.slack_team + '.slack.com/api/';
   actions[ event.action ](event, context);
 };
 
 var actions = {
   info: function(event, context) {
     var params = {
-      url: api_base + '/users.list',
+      url: api_base + 'users.list',
       qs: {
         token: event.slack_token,
         presence: 1,
       },
     };
     request.get(params, function(err, response, body) {
+      if ( err ) { return context.fail(err) }
       var info = {};
-      var json = JSON.parse(body);
-      var users = json.members.filter(function(user) { return ! user.is_bot && ! user.deleted; });
+      var users = JSON.parse(body).members.filter(function(user) { return ! user.is_bot && ! user.deleted; });
       info.total = users.length;
       info.online = users.filter(function(user) { return user.presence === 'active'; }).length;
       context.succeed(info);
@@ -33,6 +33,21 @@ var actions = {
   },
 
   invite: function(event, context) {
-    context.succeed(event);
+    if ( ! event.email ) {
+      return context.fail(new Error('undefined email'));
+    }
+    var params = {
+      url: api_base + 'users.admin.invite',
+      form: {
+        email: event.email,
+        token: event.slack_token,
+        set_active: true,
+      },
+    };
+    request.post(params, function(err, response, body) {
+      err
+        ? context.fail(err)
+        : context.succeed(JSON.parse(body));
+    });
   },
 };
